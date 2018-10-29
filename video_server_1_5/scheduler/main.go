2d2237d89eb54f4f3979168b89640d9be03ea592
bxlxwxsx113 <778136602@qq.com>
2018-10-29 22:39:57 +0800

    Merge branch 'master' of https://github.com/bxlxwxsx113/Project
    info:流媒体练习

---
+++ video_server_1_5/scheduler/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/avenssi/video_server/scheduler/taskrunner"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/video-delete-record/:vid-id", vidDelRecHandler)
+
+	return router
+}
+
+func main() {
+	go taskrunner.Start()
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)
+}
